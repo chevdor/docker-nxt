ca2168597d0361876fa897b4e3b12411b3f2197c
Wilfried Kopp <chevdor@gmail.com>
2015-07-12 11:12:57 +0200

    Trying to skip the download if the blockchain is alreadey there

--- start-nxt.sh
+++ start-nxt.sh
@@ -9,18 +9,26 @@ fi
 
 echo Database is $DB
 
+echo "---------- DEBUG ------------"
+ls -al
+echo "---------- DEBUG ------------"
+
 # if we need to bootstrap, we do that first.
 # Warning, bootstrapping will delete the current blockchain.
 # This ENV variable must be removed for the container
 # to start 'normally'.
 # $BLOCKCHAINDL must point to a zip that contains the nxt_db folder itself.
 if [ -n "${BLOCKCHAINDL-}" ] && [ ! -d "$DB" ]; then
+	echo "$DB not found, downloading blockchain from $BLOCKCHAINDL";
 	wget "$BLOCKCHAINDL" && unzip *.zip && rm *.zip
+	echo "Blockchain download complete"
 fi
 
 if [ "$NXTNET" = "main" ]; then
+	echo "Linking config to mainnet"
 	ln -s /nxt/conf/nxt-main.properties /nxt/conf/nxt.properties
 else
+	echo "Linking config to testnet"
 	ln -s /nxt/conf/nxt-test.properties /nxt/conf/nxt.properties
 fi  
 
